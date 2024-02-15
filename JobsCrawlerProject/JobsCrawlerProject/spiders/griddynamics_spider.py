51c7f6c8a8cd1f8280f61700ebac59c3c48179c9
andreireporter13 <andrei.reporter13@gmail.com>
2024-02-15 23:35:03 +0200

    readded griddynamics

--- JobsCrawlerProject/JobsCrawlerProject/spiders/griddynamics_spider.py
+++ JobsCrawlerProject/JobsCrawlerProject/spiders/griddynamics_spider.py
@@ -9,7 +9,7 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from JobsCrawlerProject.items import JobItem
 #
-import uuid
+from JobsCrawlerProject.found_county import get_county
 
 
 class GriddynamicsSpiderSpider(CrawlSpider):
@@ -25,18 +25,21 @@ class GriddynamicsSpiderSpider(CrawlSpider):
     def parse_job(self, response):
 
         # get location
-        location = response.css('div.gd-typography-body.underline-text.ng-star-inserted > div.ng-star-inserted::text').get().strip()
+        location = response.xpath('//div[@class="ng-star-inserted"]//text()').extract_first().strip()
+        if (correct_location := location.split(',')[0]).lower() == 'bucharest':
+            correct_location = "Bucuresti"
 
         # get data
         if 'romania' in location.lower():
             #
             item = JobItem()
-            item['id'] = str(uuid.uuid4())
             item['job_link'] = response.url
-            item['job_title'] = response.css('h1.gd-typography-h2.title-label::text').get().strip()
+            item['job_title'] = response.xpath('//h1[contains(@class, "title-label")]/text()').extract_first().strip()
             item['company'] = 'GridDynamics'
             item['country'] = 'Romania'
-            item['city'] = location.split(',')[0]
+            item['county'] = get_county(correct_location.title())
+            item['city'] = correct_location.title()
+            item['remote'] = 'on-site'
             item['logo_company'] = 'https://itkonekt.com/media/2019/02/2019-02-26.png'
             #
             yield item
