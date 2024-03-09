647bd50ac21934f3ff9db906f78ec9d2457095c6
andreireporter13 <andrei.reporter13@gmail.com>
2024-03-09 11:07:20 +0200

    added ninjaone to scrapy with new changes

--- JobsCrawlerProject/JobsCrawlerProject/spiders/ninjaone_spider.py
+++ JobsCrawlerProject/JobsCrawlerProject/spiders/ninjaone_spider.py
@@ -8,7 +8,7 @@
 import scrapy
 from JobsCrawlerProject.items import JobItem
 #
-import uuid
+from JobsCrawlerProject.found_county import get_county
 
 
 class NinjaoneSpiderSpider(scrapy.Spider):
@@ -25,13 +25,21 @@ class NinjaoneSpiderSpider(scrapy.Spider):
             country = job["country"]["name"]
 
             if country == "Romania":
+
+                if (location := job.get('location').strip().lower()) == 'bucharest':
+                    location = 'Bucuresti'
+                location_finish = get_county(location=location)
+
                 item = JobItem()
-                item['id'] = str(uuid.uuid4())
-                item['job_link'] = job["hostedUrl"]
-                item['job_title'] = job["title"]
+                item['job_link'] = job.get("hostedUrl")
+                item['job_title'] = job.get("title")
                 item['company'] = 'NinjaOne'
                 item['country'] = country
-                item['city'] = job["location"]
+                item['county'] = location_finish[0] if True in location_finish else None
+                item['city'] = 'all' if location.lower() == location_finish[0].lower()\
+                                    and True in location_finish and 'bucuresti' != location.lower()\
+                                        else location
+                item['remote'] = 'remote'
                 item['logo_company'] = 'https://www.logo-designer.co/storage/2022/08/2021-it-firm-ninjaone-new-logo-design.png'
                 #
                 yield item
