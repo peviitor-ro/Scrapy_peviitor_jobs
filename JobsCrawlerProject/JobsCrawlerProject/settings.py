755247de2ed6966bfa5f45f6b88b6b07f88845df
andreireporter13 <andrei.reporter13@gmail.com>
2024-09-09 01:06:46 +0300

    added added data to validator

--- JobsCrawlerProject/JobsCrawlerProject/settings.py
+++ JobsCrawlerProject/JobsCrawlerProject/settings.py
@@ -69,7 +69,7 @@ DEFAULT_REQUEST_HEADERS = {
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-#  'JobsCrawlerProject.pipelines.UpdateAPIPipeline': 1,
+  'JobsCrawlerProject.pipelines.UpdateAPIPipeline': 1,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
