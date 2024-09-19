a35d91a42ac9136ac6306e796fd60f363c3c0a1b
flori <floricatarca@yahoo.com>
2024-09-19 11:19:26 +0300

    added spiders(Arkadium, Google, Testronic) for validator

---
+++ spiders/arkadium_spider.py
@@ -0,0 +1,19 @@
+import sys
+import os
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+os.environ['SCRAPY_SETTINGS_MODULE'] = 'JobsCrawlerProject.settings'
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_dir = os.path.abspath(os.path.join(current_dir, '..', 'JobsCrawlerProject'))
+
+sys.path.append(project_dir)
+
+from JobsCrawlerProject.spiders.arkadium_spider import ArkadiumSpiderSpider
+
+if __name__ == "__main__":
+    # Initialize the crawling process with the project's settings
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(ArkadiumSpiderSpider)
+    process.start()
